2ac8bf2ef1f424ea0ed52bf98ad7d147b64e6acc
Mari <57656147+mari-esakkiraj@users.noreply.github.com>
2023-03-09 20:56:18 +0530

    profile page updated

--- web/theme/js/custom.js
+++ web/theme/js/custom.js
@@ -117,15 +117,6 @@
     $("body").removeClass("fix");
   });
 
-  // Add product
-  $(".btn-product-add,.quickViewModal").on('click', function() {
-    $(this).animate({
-      'opacity' : 0
-    }, 400, function(){
-        $(this).html('Added').animate({'opacity': 1}, 400);
-    });
-  });
-
   $(".sort-by-cover").on('click', function() {
     $(this).find(".sort-by-dropdown").toggleClass("show");
   });
