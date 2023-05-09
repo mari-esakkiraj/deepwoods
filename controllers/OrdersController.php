1ee854a215a95be2d4ecb02dac3648ec4ee5cb2c
arulrajj <51719250+arul258013@users.noreply.github.com>
2023-05-09 20:36:47 +0530

    mpdf install

--- controllers/OrdersController.php
+++ controllers/OrdersController.php
@@ -23,6 +23,7 @@ use Razorpay\Api\Api;
 use app\models\Settings;
 use Razorpay\Api\Errors\SignatureVerificationError;
 use app\models\Promotion;
+use kartik\mpdf\Pdf;
 
 /**
  * OrdersController implements the CRUD actions for Orders model.
@@ -708,10 +709,46 @@ class OrdersController extends Controller
             }
         
         }
-
-      
-    
-    
            
+    }
+
+    public function actionPdfreport($id) {
+        $html = "";
+        $order = Orders::findOne($id);
+        // get your HTML raw content without any layouts or scripts
+        $content = $this->renderPartial('pdforder',["success" => '', 'message' => $html, 'order' => $order]);
+        //$content = '<p>aaa</p>';
+        /*$mpdf = new mPDF();
+        $mpdf->WriteHTML($content);
+        $mpdf->Output();die;
+        die;*/
+        // setup kartik\mpdf\Pdf component
+        $pdf = new Pdf([
+            // set to use core fonts only
+            'mode' => Pdf::MODE_CORE, 
+            // A4 paper format
+            'format' => Pdf::FORMAT_A4, 
+            // portrait orientation
+            'orientation' => Pdf::ORIENT_PORTRAIT, 
+            // stream to browser inline
+            'destination' => Pdf::DEST_BROWSER, 
+            // your html content input
+            'content' => $content,//$content,  
+            // format content from your own css file if needed or use the
+            // enhanced bootstrap css built by Krajee for mPDF formatting 
+            //'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
+            // any css to be embedded if required
+            'cssInline' => '.kv-heading-1{font-size:18px}', 
+             // set mPDF properties on the fly
+            'options' => ['title' => 'Krajee Report Title'],
+             // call mPDF methods on the fly
+            'methods' => [ 
+                'SetHeader'=>['Krajee Report Header'], 
+                'SetFooter'=>['{PAGENO}'],
+            ]
+        ]);
+        
+        // return the pdf output as per the destination setting
+        return $pdf->render(); 
     }
 }
