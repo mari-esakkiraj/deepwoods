337e4d9b223d2058346dc34ff7625840e80df799
Mari <57656147+mari-esakkiraj@users.noreply.github.com>
2023-05-30 23:54:25 +0530

    commit

--- views/profile/change_password.php
+++ views/profile/change_password.php
@@ -2,68 +2,124 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use yii\helpers\Url;
 //  $this->title = 'Change Password';
 $this->params['breadcrumbs'][] = $this->title;
+$absoluteBaseUrl = Url::base(true);
 ?>
 <style>
-    .eyeIcon {
-        position: absolute;
-        margin-left: 93%;
-        margin-top: -3%;
-    }
-    .haserror .eyeIcon {
-        margin-top: -3%;
+    
+    .togglePasswordShow {
+        float: right;
+        margin-top: -30px;
+        margin-right: 15px;
+        cursor:pointer;
     }
 </style>
 <h1><?= Html::encode($this->title) ?></h1>
 
 <div class="user-change-password">
-    <?php $form = ActiveForm::begin(); ?>
-    <div class="row">
-        <div class="mb-3">
-            <?= $form->field($model, 'currentPassword')->passwordInput(['class' => 'form-control curpassword']) ?>
-            <i class="fa fa-fw fa-eye eyeIcon" id="toggleCurPassword"></i>
-        </div>
-        <div class="mb-3">
-            <?= $form->field($model, 'newPassword')->passwordInput(['class' => 'form-control newpassword']) ?>
-            <i class="fa fa-fw fa-eye eyeIcon" id="toggleNewPassword"></i>
-        </div>
-        <div class="mb-3">
-            <?= $form->field($model, 'confirmPassword')->passwordInput(['class' => 'form-control confirmpassword']) ?>
-            <i class="fa fa-fw fa-eye eyeIcon" id="toggleConfirmPassword"></i>
-        </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Current Password</label>
+        <input type="password" class="form-control" name="password" id="change-password">
+        <i class="fa fa-fw fa-eye togglePasswordShow"></i>
+        <span id='change-pass-error'  style="color:red;"></span>
     </div>
-
-
-
-    <div class="form-group">
-        <?= Html::submitButton('Change Password', ['class' => 'btn btn-primary']) ?>
+    <div class="mb-3">
+        <label for="password" class="form-label">New Password</label>
+        <input type="password" class="form-control" name="password" id="change-new-password">
+        <i class="fa fa-fw fa-eye togglePasswordShow"></i>
+        <span id='change-new-pass-error'  style="color:red;"></span>
     </div>
-
-    <?php ActiveForm::end(); ?>
+    <div class="mb-3">
+        <label for="password" class="form-label">Confirm Password</label>
+        <input type="password" class="form-control" name="password" id="change-new-confirm-password">
+        <i class="fa fa-fw fa-eye togglePasswordShow"></i>
+        <span id='change-confirm-pass-error' style="color:red;"></span>
+    </div>
+    <div class="text-center mt-10">
+        <button type="button" class="btn btn-primary changemypassword">Submit</button>
+    </div>
 </div>
 
 <?php 
 $this->registerJs("
- 
-$(\"#toggleCurPassword\").click(function () {
-    $(this).toggleClass(\"fa-eye fa-eye-slash\");
-    var type = $(this).hasClass(\"fa-eye-slash\") ? \"text\" : \"password\";
-    $(\".curpassword\").attr(\"type\", type);
-});
-$(\"#toggleNewPassword\").click(function () {
-    $(this).toggleClass(\"fa-eye fa-eye-slash\");
-    var type = $(this).hasClass(\"fa-eye-slash\") ? \"text\" : \"password\";
-    $(\".newpassword\").attr(\"type\", type);
-});
-$(\"#toggleConfirmPassword\").click(function () {
-    $(this).toggleClass(\"fa-eye fa-eye-slash\");
-    var type = $(this).hasClass(\"fa-eye-slash\") ? \"text\" : \"password\";
-    $(\".confirmpassword\").attr(\"type\", type);
-});
+    $(\".togglePasswordShow\").click(function () {
+        $(this).toggleClass(\"fa-eye fa-eye-slash\");
+        var type = $(this).hasClass(\"fa-eye-slash\") ? \"text\" : \"password\";
+        $(this).parent().find('input').attr('type', type);
+    });
 
-  
+    $(document).on('keyup','#change-new-confirm-password',function() {
+        var password = $('#change-new-password').val();
+        var confirmPassword = $('#change-new-confirm-password').val();
+        if (confirmPassword !== '' && password != confirmPassword) {
+            $('#change-confirm-pass-error').text('Password doesn\'t match');
+        }else{
+            if(confirmPassword == ''){
+                $('#change-confirm-pass-error').text('Password is requried');
+            }else{
+                $('#change-confirm-pass-error').text('');
+            }
+        }
+    });
+
+    $(document).on('keyup','#change-new-confirm-password',function() {
+        var password = $('#change-new-password').val();
+        var confirmPassword = $('#change-new-confirm-password').val();
+        if (confirmPassword !== '' && password != confirmPassword) {
+            $('#change-confirm-pass-error').text('Password doesn\'t match');
+        }else{
+            if(confirmPassword == ''){
+                $('#change-confirm-pass-error').text('Password is requried');
+            }else{
+                $('#change-confirm-pass-error').text('');
+            }
+        }
+    });
+
+    $(document).on('keyup','#change-password',function() {
+        var password = $('#change-password').val();
+        if(password == ''){
+            $('#change-pass-error').text('Current Password is requried');
+        }else{
+            $('#change-pass-error').text('');
+        }
+    });
+
+    $(document).on('keyup','#change-new-password',function() {
+        var password = $('#change-new-password').val();
+        if(password == ''){
+            $('#change-new-pass-error').text('New Password is requried');
+        }else{
+            $('#change-new-pass-error').text('');
+        }
+    });
+
+    $(document).on('click','.changemypassword',function() {
+        $.ajax({
+            type:'post',
+            url:'".$absoluteBaseUrl."/site/changepassword',
+            dataType: 'json',
+            data:{
+                oldpass:$('#change-password').val(),
+                newpass:$('#change-new-password').val(),
+            },
+            success:function(response) {
+                var resultData = response.success
+                if(resultData){
+                    $('#change-password').val();
+                    $('#change-new-password').val('');
+                    $('#change-new-password').val('');
+                    toastr.success('Your password has been changed successfully.');
+                }else{
+                    $('#change-pass-error').text(response.message);
+                }
+            }
+        }); 
+    });
+
+    
 ");
 
 
